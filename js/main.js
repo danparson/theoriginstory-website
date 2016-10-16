5d61119be54984109a1c11e8a38c1329da440ca8
Dan <dan@danparson.com>
2016-10-15 22:26:19 -0500

    Reorganized code and data for easier tracking and updating

--- js/main.js
+++ js/main.js
@@ -1,26 +1,29 @@
-(function() {
-  'use strict';
+---
+---
 
-  function setSplashText() {
-	var splashes = ["Now with more lyrics!", "Pet Dan's Beard!", "Craigslist Craig", "<del>&nbsp;4&nbsp;</del> 8 sausage <del>&nbsp;biscuits&nbsp;</del> McMuffins", "Cup of water, only ice", "Well that's not safe", "Red light camera ahead", "Matchbox 20 cover band", "The Midwest Ska Spectacular!"];
+(function() {
+	'use strict';
+
+	function setSplashText() {
+	var splashes = ["{{ site.data.global.splashes | join: '", "' }}"];
 	var max = splashes.length - 1;
 	var min = 0;
 	var splashPos = Math.floor(Math.random() * (max - min + 1)) + min;
 	$(".tos-navbar-brand-text").html("\"" + splashes[splashPos] + "\"");
-  }
+	}
 
-  $(document).ready(function() {
-    setSplashText();
-  });
-  
-  $('.bio-photo-panel a').on('click', function() {
+	$(document).ready(function() {
+		setSplashText();
+	});
+
+	$('.bio-photo-panel a').on('click', function() {
 	$(".list-group-item").removeClass("active");
 	var addressValue = $(this).attr("href");
-    $(addressValue).parent().addClass("active");
-  });
+		$(addressValue).parent().addClass("active");
+	});
 
-  $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
-  });
+	$(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+		event.preventDefault();
+		$(this).ekkoLightbox();
+	});
 })();
